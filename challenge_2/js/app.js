6651b4aabdebf9cb77665d195760cef0d0b34028
rhamill1 <rhamill11@gmail.com>
2016-10-04 15:34:59 -0700

    challenges 1-3

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,29 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+
+function WelcomeController(){
+  this.full_name = "Pizza Monster";
+  this.age = 30;
+  this.city = "Bel Air";
+  this.nameL = this.full_name.length;
+  this.shout = function(n) {
+    return n.toUpperCase() + '!'
+  }
+}
+
+
+function ClassController(){
+  this.class_name = "WDI 32";
+  this.enrolled_students = 13;
+  this.start_date = "2016-09-06";
+  this.end_date = "2016-11-30";
+  this.dd = function(n) {
+    return start_date.diff(end_date, 'days');
+  }
+
+
+}
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,29 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController)
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
+
+
+app.controller("plusController", plusController)
+
+function plusController(){
+  this.num1 = 1;
+  this.num2 = 2;
+  this.num3 = 3;
+  this.num4 = 4;
+}
+
+
+app.controller("loanController", loanController)
+
+function loanController(){
+  this.amount = 10000;
+  this.apr = 5;
+  this.accrued_interest = this.amount * (this.apr/100);
+  this.total_balance = this.amount + this.accrued_interest;
+}
